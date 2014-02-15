d5e424a58caf235766d28bdca6d23b1a26a126e4
R. Lemos <rslemos@gmail.com>
2014-02-15 15:22:14 -0200

    [webmixer] ignoring executables

--- webmixer/httpd.c
+++ webmixer/httpd.c
@@ -1,3 +1,24 @@
+/*******************************************************************************
+ * BEGIN COPYRIGHT NOTICE
+ * 
+ * This file is part of program "I-Trigue 2.1 3300 Digital Control"
+ * Copyright 2013-2014  R. Lemos
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * END COPYRIGHT NOTICE
+ ******************************************************************************/
 #include <string.h>
 #include <jansson.h>
 #include <microhttpd.h>
